f739620767b9c964e158410b38201d953be1ad20
Badvelu Haripriya <Haripriya.Badvelu@Ltts.com>
2020-10-21 14:47:22 +0530

    assignment05

--- Assignment03/FurnitureApp.java
+++ Assignment03/FurnitureApp.java
@@ -1,12 +1,12 @@
 import com.javapeople.data.*; //check this package for unqualified class names.
-import in.ys.tejaswini.data.*;
+import in.Badvelu.Haripriya.data.*;
 
 class FurnitureApp{
     public static void main(String []args){
 
             com.javapeople.data.List list1=new com.javapeople.data.List();
             System.out.println(list1);
-            in.ys.tejaswini.data.List list2= new in.ys.tejaswini.data.List();
+            in.Badvelu.Haripriya.data.List list2= new in.Badvelu.Haripriya.data.List();
             System.out.println(list2);
             list2.add();
             list1.append();
